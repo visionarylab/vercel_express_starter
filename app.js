c4ecf2bf55eafd3386f35143a2f992afdce92179
visionarylab <visionarylabs@gmail.com>
2024-06-01 03:13:39 +0000

    ytdl-core

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const ytdl = require('ytdl-core');
+const cors = require('cors');
 const express = require('express');
 
 const app = express()
@@ -10,6 +12,17 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   res.send('About route 🎉 ')
 })
+
+app.get('/download', (req,res) => {
+    var URL = req.query.URL;
+    URL = "https://www.youtube.com/watch?v=XkOXNlHJP6M";
+    res.header('Content-Disposition', 'attachment; filename="video.mp4"');
+    ytdl(URL, {
+        format: 'mp4'
+        }).pipe(res);
+        
+});
+    
 
 app.listen(PORT, () => {
   console.log(`✅ Server is running on port ${PORT}`);
