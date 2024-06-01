9c2862dba52f365367285e03bef95af1d14b6cb0
visionarylab <visionarylabs@gmail.com>
2024-06-01 03:59:51 +0000

    trick or

--- api/api.js
+++ api/api.js
@@ -1,4 +1,11 @@
+const ytdl = require('ytdl-core');
+
 export default function handler(request, response) {
     const { name = 'World' } = request.query;
-    return response.send(`Hello ${name}!`);
+    var URL = req.query.URL;
+    URL = "https://www.youtube.com/watch?v=oQ78byewHWY";
+    response.header('Content-Disposition', 'attachment; filename="video.mp4"');
+    ytdl(URL, {
+        format: 'mp4'
+        }).pipe(response);
 }
